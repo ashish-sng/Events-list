dcaf114ac1dcd82cfb97392e2738b1f79e2e3453
Ashish Singh <ashishsingh.1899@gmail.com>
2023-06-27 20:56:14 +0530

    added event-list and event-item components

---
+++ components/events/event-list.js
@@ -0,0 +1,14 @@
+import EventItem from "./event-item";
+
+const EventList = (props) => {
+  const { items } = props;
+  return (
+    <ul>
+      {items.map((event) => (
+        <EventItem event={event} />
+      ))}
+    </ul>
+  );
+};
+
+export default EventList;
