a61acdcaf0d9d8a3f12594b2687b81ca651e7dc2
Ashish Singh <ashishsingh.1899@gmail.com>
2023-06-29 18:09:58 +0530

    updated dynamic routing

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,35 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import { getEventById } from "@/dummy-data";
+import EventSummary from "@/components/event-detail/event-summary";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventContent from "@/components/event-detail/event-content";
+
 const EventDetailPage = () => {
-    return (
-        <div>
-        <h1>Event Detail Page</h1>
-        </div>
-    );
-}
-    
+  const router = useRouter();
+
+  router.query.eventId;
+
+  const event = getEventById(router.query.eventId);
+  console.log(event);
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
+};
+
 export default EventDetailPage;
