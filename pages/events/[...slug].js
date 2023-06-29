c9d3f960873f58cabc053ed8a8a50183fc8a64d3
Ashish Singh <ashishsingh.1899@gmail.com>
2023-06-29 19:44:33 +0530

    added filtered page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,9 +1,68 @@
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
+import EventList from "@/components/events/event-list";
+import { Fragment } from "react";
+import ResultsTitle from "@/components/events/results-title";
+import Button from "@/components/ui/button";
+import ErrorAlert from "@/components/ui/error-alert";
+
 const FilteredEventsPage = () => {
+  const router = useRouter();
+  console.log(router.query);
+
+  const { slug } = router.query;
+  if (!slug) {
+    return <p className="center">Loading...</p>;
+  }
+  const filteredYear = slug[0];
+  const filteredMonth = slug[1];
+  const filteredEvents = getFilteredEvents({
+    year: filteredYear,
+    month: filteredMonth,
+  });
+  console.log(filteredEvents);
+
+  if (
+    isNaN(filteredYear) ||
+    isNaN(filteredMonth) ||
+    filteredYear > 2030 ||
+    filteredYear < 2021 ||
+    filteredMonth < 1 ||
+    filteredMonth > 12
+  ) {
     return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
         <div>
-        <h1>Filtered Events</h1>
+          <Button link="/events">Show all events</Button>
         </div>
+      </Fragment>
     );
-}
-    
-export default FilteredEventsPage; 
+  }
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show all events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(filteredYear, filteredMonth - 1); // month is 0 based
+
+  return (
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
+  );
+};
+
+export default FilteredEventsPage;
